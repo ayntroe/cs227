f0b678da180ae362698299fa748c9270b3862529
Alain Yount-Roe <yountroe@iastate.edu>
2013-10-17 20:19:47 -0700

    UO Inscription and Bookfiller source

---
+++ Desktop/COMS/macropackage/spelltable.lua
@@ -0,0 +1,82 @@
+circlemana = {4,6,9,11,14,20,40,50}
+
+spells = {
+        --Circle One--
+         {name = "Reactive Armor", circle = 1,spell = 1, type = 7981},
+         {name = "Clumsy", circle = 1,spell = 2, type = 7982},
+         {name = "Create Food", circle = 1,spell = 3, type = 7983},
+         {name = "Feeblemind", circle = 1,spell = 4, type = 7984},
+         {name = "Heal", circle = 1,spell = 5, type = 7985},
+         {name = "Magic Arrow", circle = 1,spell = 6, type = 7986},
+         {name = "Night Sight", circle = 1,spell = 7, type = 7987},
+         {name = "Weaken", circle = 1,spell = 8, type = 7988},
+         
+         --Circle Two--
+         {name = "Agility", circle = 2,spell = 1, type = 7989},
+         {name = "Cunning", circle = 2,spell = 2, type = 7990},
+         {name = "Cure", circle = 2,spell = 3, type = 7991},
+         {name = "Harm", circle = 2,spell = 4, type = 7992},
+         {name = "Magic Trap", circle = 2,spell = 5, type = 7993},
+         {name = "Magic Untrap", circle = 2,spell = 6, type = 7994},
+         {name = "Protection", circle = 2,spell = 7, type = 7995},
+         {name = "Strength", circle = 2,spell = 8, type = 7996},
+         
+         --Circle Three--
+         {name = "Bless", circle = 3,spell = 1, type = 7997},
+         {name = "Fireball", circle = 3,spell = 2, type = 7998},
+         {name = "Magic Lock", circle = 3,spell = 3, type = 7999},
+         {name = "Poison", circle = 3,spell = 4, type = 8000},
+         {name = "Telekenesis", circle = 3,spell = 5, type = 8001},
+         {name = "Teleport", circle = 3,spell = 6, type = 8002},
+         {name = "Unlock", circle = 3,spell = 7, type = 8003},
+         {name = "Wall Of Stone", circle = 3,spell = 8, type = 8004},
+         
+         --Circle Four--
+         {name = "Arch Cure", circle = 4,spell = 1, type = 8005},
+         {name = "Arch Protection", circle = 4,spell = 2, type = 8006},
+         {name = "Curse", circle = 4,spell = 3, type = 8007},
+         {name = "Fire Field", circle = 4,spell = 4, type = 8008},
+         {name = "Greater Heal", circle = 4,spell = 5, type = 8009},
+         {name = "Lightning", circle = 4,spell = 6, type = 8010},
+         {name = "Mana Drain", circle = 4,spell = 7, type = 8011},
+         {name = "Recall", circle = 4,spell = 8, type = 8012},
+         
+         --Circle Five--
+         {name = "Blade Spirits", circle = 5,spell = 1, type = 8013},
+         {name = "Dispel Field", circle = 5,spell = 2, type = 8014},
+         {name = "Incognito", circle = 5,spell = 3, type = 8015},
+         {name = "Magic Reflection", circle = 5,spell = 4, type = 8016},
+         {name = "Mind Blast", circle = 5,spell = 5, type = 8017},
+         {name = "Paralyze", circle = 5,spell = 6, type = 8018},
+         {name = "Poison Field", circle = 5,spell = 7, type = 8019},
+         {name = "Summon Creature", circle = 5,spell = 8, type = 8020},
+         
+         --Circle Six--
+         {name = "Dispel", circle = 6,spell = 1, type = 8021},
+         {name = "Energy Bolt", circle = 6,spell = 2, type = 8022},
+         {name = "Explosion", circle = 6,spell = 3, type = 8023},
+         {name = "Invisibility", circle = 6,spell = 4, type = 8024},
+         {name = "Mark", circle = 6,spell = 5, type = 8025},
+         {name = "Mass Curse", circle = 6,spell = 6, type = 8026},
+         {name = "Paralyze Field", circle = 6,spell = 7, type = 8027},
+         {name = "Reveal", circle = 6,spell = 8, type = 8028},
+         
+         --Circle Seven--
+         {name = "Chain Lightning", circle = 7,spell = 1, type = 8029},
+         {name = "Energy Field", circle = 7,spell = 2, type = 8030},
+         {name = "Flamestrike", circle = 7,spell = 3, type = 8031},
+         {name = "GateTravel", circle = 7,spell = 4, type = 8032},
+         {name = "Mana Vampire", circle = 7,spell = 5, type = 8033},
+         {name = "Mass Dispel", circle = 7,spell = 6, type = 8034},
+         {name = "Meteor Swarm", circle = 7,spell = 7, type = 8035},
+         {name = "Polymorph", circle = 7,spell = 8, type = 8036},
+         
+         --Circle Eight--
+         {name = "Earthquake", circle = 8,spell = 1, type = 8037},
+         {name = "Energy Vortex", circle = 8,spell = 2, type = 8038},
+         {name = "Resurrection", circle = 8,spell = 3, type = 8039},
+         {name = "Air Elemental", circle = 8,spell = 4, type = 8040},
+         {name = "Daemon", circle = 8,spell = 5, type = 8041},
+         {name = "Earth Elemental", circle = 8,spell = 6, type = 8042},
+         {name = "Fire Elemental", circle = 8,spell = 7, type = 8043},
+         {name = "Water Elemental", circle = 8,spell = 8, type = 8044}}
